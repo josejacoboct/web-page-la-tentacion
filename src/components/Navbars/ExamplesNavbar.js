faf4262df7323091bb17f4cf1abfac5242424273
José Jacobo Contreras Trejo <josejacobo@MBP-de-Jose.lan>
2020-06-11 13:47:40 -0500

    ojala funcione

---
+++ src/components/Navbars/ExamplesNavbar.js
@@ -0,0 +1,132 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+// nodejs library that concatenates strings
+import classnames from "classnames";
+
+// reactstrap components
+import {
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container
+} from "reactstrap";
+
+function ExamplesNavbar() {
+  const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
+  const [navbarCollapse, setNavbarCollapse] = React.useState(false);
+
+  const toggleNavbarCollapse = () => {
+    setNavbarCollapse(!navbarCollapse);
+    document.documentElement.classList.toggle("nav-open");
+  };
+
+  React.useEffect(() => {
+    const updateNavbarColor = () => {
+      if (
+        document.documentElement.scrollTop > 299 ||
+        document.body.scrollTop > 299
+      ) {
+        setNavbarColor("");
+      } else if (
+        document.documentElement.scrollTop < 300 ||
+        document.body.scrollTop < 300
+      ) {
+        setNavbarColor("navbar-transparent");
+      }
+    };
+
+    window.addEventListener("scroll", updateNavbarColor);
+
+    return function cleanup() {
+      window.removeEventListener("scroll", updateNavbarColor);
+    };
+  });
+  return (
+    <Navbar
+      className={classnames("fixed-top", navbarColor)}
+      color-on-scroll="300"
+      expand="lg"
+    >
+      <Container>
+        <div className="navbar-translate">
+          <NavbarBrand
+            data-placement="bottom"
+            to="/index"
+            target="_blank"
+            title="La tentación"
+            tag={Link}
+          >
+            La tentación
+          </NavbarBrand>
+          <button
+            aria-expanded={navbarCollapse}
+            className={classnames("navbar-toggler navbar-toggler", {
+              toggled: navbarCollapse
+            })}
+            onClick={toggleNavbarCollapse}
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className="justify-content-end"
+          navbar
+          isOpen={navbarCollapse}
+        >
+          <Nav navbar>
+           
+            <NavItem>
+              <NavLink
+                href ="/productsPage"
+                target="_blank"
+              >
+                <i className="nc-icon nc-book-bookmark" /> Productos
+              </NavLink>
+            </NavItem>
+            
+            
+            <NavItem>
+              <NavLink
+                data-placement="bottom"
+                href="https://www.instagram.com/la.tentacion_/"
+                target="_blank"
+                title="Follow us on Instagram"
+              >
+                <i className="fa fa-instagram" />
+                <p className="d-lg-none">Instagram</p>
+                <label>Instagram</label>
+              </NavLink>
+             
+            </NavItem>
+            
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default ExamplesNavbar;
---
+++ src/views/index-sections/SectionCarousel.js
@@ -0,0 +1,164 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselIndicators,
+  CarouselCaption
+} from "reactstrap";
+
+// core components
+
+const items = [
+  {
+    src: require("assets/img/pasteles/pastelZanahoria.jpeg"),
+    altText: "Pastel de zanahoria",
+    caption: "Pastel de zanahoria"
+  },
+  {
+    src: require("assets/img/pasteles/pastelQuesoFresasMachacadas.jpeg"),
+    altText: "Pastel de queso con fresas machacadas",
+    caption: "Pastel de queso con fresas machacadas"
+  },
+  {
+    src: require("assets/img/pasteles/edenChocolate.jpeg"),
+    altText: "Edén de chocolate",
+    caption: "Edén de chocolate"
+  },
+  {
+    src: require("assets/img/pasteles/profiteroles.jpeg"),
+    altText: "Profiteroles",
+    caption: "Profiteroles"
+  },
+  {
+    src: require("assets/img/pasteles/pastelAleman.jpeg"),
+    altText: "Pastel Alemán",
+    caption: "Pastel Alemán"
+  },
+  {
+    src: require("assets/img/pasteles/pastelQuesoMocha.jpeg"),
+    altText: "Pastel de queso sabor mocha",
+    caption: "Pastel de queso sabor mocha"
+  },
+  {
+    src: require("assets/img/pasteles/payManzana.jpeg"),
+    altText: "Pay de manzana",
+    caption: "Pay de manzana"
+  }  
+];
+
+function SectionCarousel() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [animating, setAnimating] = React.useState(false);
+  const onExiting = () => {
+    setAnimating(true);
+  };
+  const onExited = () => {
+    setAnimating(false);
+  };
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+  const goToIndex = newIndex => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+  return (
+    <>
+      <div className="section pt-o" id="carousel">
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto" md="10">
+              <Card className="page-carousel">
+                <Carousel
+                  activeIndex={activeIndex}
+                  next={next}
+                  previous={previous}
+                >
+                  <CarouselIndicators
+                    items={items}
+                    activeIndex={activeIndex}
+                    onClickHandler={goToIndex}
+                  />
+                  {items.map(item => {
+                    return (
+                      <CarouselItem
+                        onExiting={onExiting}
+                        onExited={onExited}
+                        key={item.src}
+                      >
+                        <img src={item.src} alt={item.altText}/>
+                        <CarouselCaption
+                          //captionText={item.caption}
+                          captionHeader={item.caption}
+                        />
+                      </CarouselItem>
+                    );
+                  })}
+                  <a
+                    className="left carousel-control carousel-control-prev"
+                    data-slide="prev"
+                    href="#pablo"
+                    onClick={e => {
+                      e.preventDefault();
+                      previous();
+                    }}
+                    role="button"
+                  >
+                    <span className="fa fa-angle-left" />
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a
+                    className="right carousel-control carousel-control-next"
+                    data-slide="next"
+                    href="#pablo"
+                    onClick={e => {
+                      e.preventDefault();
+                      next();
+                    }}
+                    role="button"
+                  >
+                    <span className="fa fa-angle-right" />
+                    <span className="sr-only">Next</span>
+                  </a>
+                </Carousel>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>{" "}
+    </>
+  );
+}
+
+export default SectionCarousel;
